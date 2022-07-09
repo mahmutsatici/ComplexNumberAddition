f9362c23496eaf3dac2d04deeb69c00596582ca5
Mahmut Satici <st_falcon@outlook.com>
2022-07-09 17:56:50 +0300

    Proje dosyası ekle.

---
+++ Karmasik_Sayi_Toplama/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karmasik_Sayi_Toplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Komplex n = new Komplex(3,2);
+            Komplex m = new Komplex(3,2);
+            n.yazdir(m, n);
+            Console.ReadLine();
+        }
+    }
+    class Komplex
+    {
+        public int sanalkisim { get; set; }
+        public int gercekkisim { get; set; }
+
+        public Komplex(int _gercekkisim, int _sanalkisim)
+        {
+            this.sanalkisim = _sanalkisim;
+            this.gercekkisim = _gercekkisim;
+        }
+        public static string operator +(Komplex a, Komplex b)
+        {
+            int sanaltoplam = a.sanalkisim + b.sanalkisim;
+            int gercektoplam = a.gercekkisim + b.gercekkisim;
+            return sanaltoplam + " + (" + gercektoplam + "i)";
+
+        }
+        public static bool operator ==(Komplex a, Komplex b)
+        {
+            if (a.gercekkisim==b.gercekkisim && a.sanalkisim==b.sanalkisim)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Komplex a, Komplex b)
+        {
+            if (a.gercekkisim != b.gercekkisim && a.sanalkisim != b.sanalkisim)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void yazdir(Komplex a,Komplex b)
+        {
+            if (a==b)
+            {
+                Console.WriteLine("Komplex sayılar eşittir");
+            }
+            else
+            {
+                Console.WriteLine("Komplex sayılar eşit değildir");
+            }
+            Console.WriteLine("Komplex sayıların toplamı: "+(a+b));
+        }
+    }
+}
---
+++ Karmasik_Sayi_Toplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Karmasik_Sayi_Toplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Karmasik_Sayi_Toplama")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("336685e9-d839-4fa3-93db-70960c5e19dc")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
